170983b5a19d441486a093c0e2d5741fd9250b84
Ermias Wakgari Yadeta <ermiaswakgarisse5@gmail.com>
2025-08-20 21:46:34 +0300

    client: configured talwind more

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline text-red-500">
+        <h1 className="text-3xl font-bold underline text-primary">
           Hello world!
         </h1>
       </div>
