9d95e510e824533e61c2df2aa4f55aba72b05e96
Ermias Wakgari Yadeta <ermiaswakgarisse5@gmail.com>
2025-08-28 11:06:59 +0300

    edited appError

--- server/utils/appError.js
+++ server/utils/appError.js
@@ -1,9 +1,8 @@
-// utils/appError.js
 export class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = true; // distinguish known vs unknown errors
+    this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
 }
