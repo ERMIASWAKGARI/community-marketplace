d0154b2104a0964d0b7a64194c4c44ceb22e5870
Ermias Wakgari Yadeta <ermiaswakgarisse5@gmail.com>
2025-08-28 10:02:32 +0300

    edited user routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 import express from "express";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    status: "success",
-    message: "Hello World, from the community marketplace API!",
-  });
-});
+app.use(express.json());
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,6 +4,15 @@ export const getUser = (req, res) => {
 
 export const createUser = (req, res) => {
   // Logic to create a new user
+
+  const newUser = req.body;
+  // Save the new user to the database
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
 };
 
 export const updateUser = (req, res) => {
@@ -12,4 +21,8 @@ export const updateUser = (req, res) => {
 
 export const deleteUser = (req, res) => {
   // Logic to delete a user by ID
+};
+
+export const getAllUsers = (req, res) => {
+  // Logic to get all users
 };
